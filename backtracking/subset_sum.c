e4e61dca98b3d685f99b55f24492ae6d4df9ab1c
yu <fengyu225@gmail.com>
2014-04-08 18:46:30 -0700

    subset sum: working

---
+++ backtracking/subset_sum.c
@@ -0,0 +1,15 @@
+/*
+ Subset sum problem is to find subset of elements that are selected from a given set whose sum adds up to a given number K. We are considering the set contains non-negative values. It is assumed that the input set is unique (no duplicates are presented).
+ *
+ */
+
+#include<stdio.h>
+
+void ss(int* arr, int size, int curr, int* t, int t_curr, int k){
+    
+}
+
+int main(){
+
+    return 0;
+}
