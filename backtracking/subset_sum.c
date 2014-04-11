7dd809391d7824a6ba299e1f0743202f0040422b
yu <fengyu225@gmail.com>
2014-04-10 17:03:05 -0700

    subset sum: done

--- backtracking/subset_sum.c
+++ backtracking/subset_sum.c
@@ -5,11 +5,26 @@
 
 #include<stdio.h>
 
-void ss(int* arr, int size, int curr, int* t, int t_curr, int k){
-    
+void ss(int* arr, int size, int curr, int* t, int t_curr, int curr_sum, int target){
+    if(curr == size)
+        return;
+    if(curr_sum+arr[curr] == target){
+        t[t_curr++] = arr[curr];
+        for(int i=0; i<t_curr; i++)
+            printf("%d ", t[i]);
+        printf("\n");
+    }
+    if(curr_sum+arr[curr]<target){
+        t[t_curr] = arr[curr];
+        ss(arr, size, curr+1, t, t_curr+1, curr_sum+arr[curr], target);
+    }
+    ss(arr, size, curr+1, t, t_curr, curr_sum, target);
 }
 
 int main(){
-
+    int arr[] = {10, 7, 5, 18, 12, 20, 15};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int t[7];
+    ss(arr,size,0,t,0,0,35);
     return 0;
 }
