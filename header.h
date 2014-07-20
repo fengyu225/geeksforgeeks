79664b7967115ca23313c5f817feb4536f9c2639
yufeng <fengyu225@gmail.com>
2014-07-19 22:26:34 -0700

    A  header.h A  test.c A  test.cpp

---
+++ header.h
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+#include<vector>
+#include<string>
+#include<queue>
+#include<stack>
+#include<unordered_map>
+#include<unordered_set>
+#include<limits.h>
+#include<algorithm>
+#include<utility>
+
+using std::vector;
+using std::string;
+using std::queue;
+using std::stack;
+using std::unordered_map;
+using std::unordered_set;
+using std::sort;
+using std::pair;
+using std::make_pair;
+using std::max;
+using std::priority_queue;
