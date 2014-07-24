b3c3aea786d3b19164156bc1ffe009eaa1c8d2ff
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-23 17:51:43 -0700

    A  magazine_cut_words.cpp

---
+++ magazine_cut_words.cpp
@@ -0,0 +1,1 @@
+Q2. I want to write a ransom note. But I don’t want to write it by hand, I’m going to cut letters out of a magazine. How can I tell if the magazine has enough of the right words to spell out the note I want to write? Let’s assume we have a way to digitize the text of the magazine. 
