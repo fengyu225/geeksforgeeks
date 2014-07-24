020e827af6eee074683572a117612b08c427d940
fengyu225@gmail.com <fengyu225@gmail.com>
2014-07-23 18:37:27 -0700

    M  magazine_cut_words.cpp A  word_break_ii.cpp

--- magazine_cut_words.cpp
+++ magazine_cut_words.cpp
@@ -1,1 +1,9 @@
-Q2. I want to write a ransom note. But I don’t want to write it by hand, I’m going to cut letters out of a magazine. How can I tell if the magazine has enough of the right words to spell out the note I want to write? Let’s assume we have a way to digitize the text of the magazine. 
+/*
+ * Q2. I want to write a ransom note. But I don’t want to write it by hand, I’m going to cut letters out of a magazine. How can I tell if the magazine has enough of the right words to spell out the note I want to write? Let’s assume we have a way to digitize the text of the magazine. 
+ *
+ */
+
+/* word break problem in leetcode
+ */
+
+
