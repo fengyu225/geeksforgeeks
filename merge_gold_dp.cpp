6b16028fd558533051e72ee0028d3fb5c288b909
yu <fengyu225@gmail.com>
2014-07-25 18:40:47 -0700

    M  merge_gold_dp.cpp

--- merge_gold_dp.cpp
+++ merge_gold_dp.cpp
@@ -1,33 +1,48 @@
 /*
  * We have n gold coins. We need to amalgamate all the n coins to create one single coin, we can merge two coins at once. The cost of merging two coins is equal to the value of those coins. How do we ensure that the cost of merging n coins in minimum.
+ *
+ *Ex: 5 ,8 , 4, 3, 9, 6
+ We will merge 3 and 4, cost=7 {Remaining coins: 5,8,9, 6,7}
+ Then we merge 5 and 6, cost=11 { Remaining coins: 11,8,9,7}
+ Then we merge 7 and 8, cost=15 { Remaining coins: 11,15,9}
+ Then we merge 9 and 11, cost=20 { Remaining coins: 20,15}
+ Then we merge 20 and 15, cost=35 { Remaining coins: 35}
+ Total cost: 7+11+15+20+35 = 88
+
+ If we had merged the coin array {5, 8, 4, 3, 9, 6} in different fashion:
+ Merging 5 and 8, cost=13 {Remaining coins: 13, 4, 3, 9, 6}
+ Merging 13 and 4, cost=17 {Remaining coins: 17, 3, 9, 6}
+ Merging 17 and 3, cost=20 {Remaining coins: 20, 9, 6}
+ Merging 20 and 9, cost=29 {Remaining coins: 29, 6}
+ Merging 29 and 6, cost=35 {Remaining coins: 35}
+ Total cost: 114
+ *
  */
 
 #include "header.h"
 
+class comp{
+    public:
+        bool operator() (int a, int b){
+            return b<=a;
+        }
+};
+
 int get_min_merge_cost(int coins[], int size){
     int res = 0;
-    for(int i=0; i<size; i++)
-        res += coins[i];
+    priority_queue<int,vector<int>,comp> q;
+    for(int i=0; i<size; i++) q.push(coins[i]);
+    while(!q.empty()){
+        int a = q.top();
+        q.pop();
+        if(q.empty()) break;
+        int b = q.top();
+        q.pop();
+        res += a+b;
+        q.push(a+b);
+    }
     return res;
 }
-
-    
-//int get_min_merge_cost(int coins[], int size){
-//    int arr[size][size+1];
-//    memset(arr,0,sizeof(arr));
-//    for(int i=0; i<size; i++) arr[i][1] = coins[i];
-//    for(int i=2; i<=size; i++){
-//        for(int j=0; j+i<=size;j++){
-//            int x = INT_MAX;
-//            for(int k=1;k<i;k++){
-//                int m = arr[j][k]+arr[j+k][i-k];
-//                x = std::min(m,x);
-//            }
-//            arr[j][i] = x;
-//        }
-//    }
-//    return arr[0][size];
-//}
 
 int main(){
     int coins[] = {5 ,8 , 4, 3, 9, 6};
