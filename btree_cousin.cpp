15fe9ee6073c314e3cc10c925e834e4d6a112692
yufeng <fengyu225@gmail.com>
2014-07-27 19:37:37 -0700

    A  btree_cousin.cpp

---
+++ btree_cousin.cpp
@@ -0,0 +1,63 @@
+#include "header.h"
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+bool is_cousin(TreeNode* root, TreeNode* a, TreeNode* b){
+    if(!a || !b || !root || a==b) return false;
+    queue<TreeNode*> curr_l, next_l;
+    curr_l.push(root);
+    queue<TreeNode*> qs[2] = {curr_l, next_l};
+    int curr = 0;
+    int found=0;
+    while(!qs[curr%2].empty()){
+        TreeNode* x = qs[curr%2].front();
+        qs[curr%2].pop();
+        if(x->left == a && x->right == b || x->left == b && x->right == a) break;
+        if(x->left){
+            qs[(curr+1)%2].push(x->left);
+            if(x->left == a || x->left == b) found++;
+        }
+        if(x->right){
+            qs[(curr+1)%2].push(x->right);
+            if(x->right == a || x->right == b) found++;
+        }
+        if(qs[curr%2].empty()){
+            if(found == 2) return true;
+            if(found == 1) break;
+            curr++;
+            found = 0;
+        }
+    }
+    return false;
+}
+
+//vector<TreeNode*> get_cousins(TreeNode* root, TreeNode* a){
+//
+//}
+
+int main(){
+    struct TreeNode n4(4);
+    struct TreeNode n5(5);
+    struct TreeNode n7(7);
+    struct TreeNode n8(8);
+    struct TreeNode n9(9);
+    struct TreeNode n2(2);
+    struct TreeNode n3(3);
+    struct TreeNode n1(1);
+    n1.left = &n2;
+    n1.right = &n3;
+    n2.left = &n4;
+    n2.right = &n5;
+    n3.right = &n7;
+    n7.right = &n8;
+    n5.right = &n9;
+    bool res = is_cousin(&n1, &n4, &n5);
+    cout<<res<<endl;
+    return 0;
+}
