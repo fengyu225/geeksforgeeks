c2879340148376a33cd75bd2bdd0bfd07e5c0025
yu <fengyu225@gmail.com>
2014-07-28 18:19:14 -0700

    A  longest_substr_without_repeating_char.cpp

---
+++ longest_substr_without_repeating_char.cpp
@@ -0,0 +1,16 @@
+#include "header.h"
+
+char* longestUniqueSubstr(char* str){
+    char* s;
+    return s;
+}
+
+int main()
+{
+    char str[] = "ABDEFGABEF";
+    printf("The input string is %s \n", str);
+    char* res =  longestUniqueSubstr(str);
+    cout<<"The length of the longest non-repeating character substring is "<<res<<endl;
+    getchar();
+    return 0;
+}
