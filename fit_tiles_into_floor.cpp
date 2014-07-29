dea9e0b15ef930f2452856d7698d19fcba2750bf
yu <fengyu225@gmail.com>
2014-07-29 12:08:25 -0700

    M  fit_tiles_into_floor.cpp

--- fit_tiles_into_floor.cpp
+++ fit_tiles_into_floor.cpp
@@ -6,12 +6,19 @@
 
 int get_num(int w){
     if(w==0 || w==1) return w;
-    int arr[2][w];
+    int arr[2][w+1];
     memset(arr,0,sizeof(arr));
-
+    arr[2][0] = 1;
+    arr[0][2] = 1;
+    for(int i=1; i<=2; i++){
+        for(int j=1; j<=w; j++){
+            arr[i][j] = arr[i-1][j]+arr[i][j-1];
+        }
+    }
+    return arr[2][w];
 }
 
 int main(){
-    cout<<get_num(2)<<endl;
+    cout<<get_num(6)<<endl;
     return 0;
 }
