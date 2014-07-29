cdd7a44593448ad56ea47de79669e07d9a194218
yu <fengyu225@gmail.com>
2014-07-29 16:12:51 -0700

    M  binary_tree_vertical_order.cpp

--- binary_tree_vertical_order.cpp
+++ binary_tree_vertical_order.cpp
@@ -6,9 +6,30 @@ struct TreeNode {
  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+void search(TreeNode* curr, int k, map<int,vector<TreeNode*> >& mp){
+    if(!curr) return;
+    if(mp.find(k) == mp.end()){
+        vector<TreeNode*> v;
+        v.push_back(curr);
+        mp[k] = v;
+    }
+    else{
+        mp[k].push_back(curr);
+    }
+    search(curr->left,k-1,mp);
+    search(curr->right,k+1,mp);
+}
+
 vector<vector<TreeNode*> > print_vertical(TreeNode* root){
     vector<vector<TreeNode*> > res;
     if(!root) return res;
+    map<int,vector<TreeNode*> > mp;
+    search(root,0,mp);
+    map<int,vector<TreeNode*> >::iterator iter = mp.begin();
+    for(;iter != mp.end(); iter++){
+        res.push_back(iter->second);
+    }
+    return res;
 }
 
 int main(){
@@ -26,7 +47,7 @@ int main(){
     vector<vector<TreeNode*> > res = print_vertical(&n1);
     for(int i=0; i<res.size(); i++){
         for(int j=0;j<res[i].size();j++)
-            cout<<res[i][j]<<" ";
+            cout<<res[i][j]->val<<" ";
         cout<<endl;
     }
     return 0;
