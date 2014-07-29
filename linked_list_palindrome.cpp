359d4344f4ed046aad3a61b3b68c8bd0cba9a3a8
yu <fengyu225@gmail.com>
2014-07-28 18:12:23 -0700

    M  linked_list_palindrome.cpp

--- linked_list_palindrome.cpp
+++ linked_list_palindrome.cpp
@@ -42,14 +42,47 @@ bool is_palindrome_stack(ListNode* root){
     return true;
 }
 
-bool is_palindrome(ListNode* root){
+ListNode* reverse(ListNode* head){
+    if(!head || !head->next) return head;
+    ListNode* curr = head;
+    ListNode* next = curr->next;
+    curr->next = NULL;
+    while(next){
+        ListNode* temp = next->next;
+        next->next = curr;
+        curr = next;
+        next = temp;
+    }
+    return curr;
+}
 
+bool is_palindrome(ListNode* root){
+    if(!root) return false;
+    if(!root->next) return true;
+    ListNode* slow = root;
+    ListNode* fast = root;
+    while(fast){
+        fast = fast->next;
+        if(!fast) break;
+        fast = fast->next;
+        slow = slow->next;
+    }
+    ListNode* second_head = reverse(slow->next);
+    ListNode* curr1 = root;
+    ListNode* curr2 = second_head;
+    while(curr2){
+        if(curr1->val != curr2->val) return false;
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    slow->next = reverse(second_head);
+    return true;
 }
 
 int main(){
-    int arr[] = {0, 1, 2, 3, 2, 1, 0};
+    int arr[] = {0, 1, 2, 9, 2, 1, 0};
     ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]));
-    bool r = is_palindrome_stack(l);
+    bool r = is_palindrome(l);
     printf("%s\n", r?"true":"false");
     return 0;
 }
