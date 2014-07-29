de020e2f09efaa6b5587fed25abdf9feed14eba1
yu <fengyu225@gmail.com>
2014-07-29 11:32:14 -0700

    M  sorted_k_almost_sorted_arr.cpp

--- sorted_k_almost_sorted_arr.cpp
+++ sorted_k_almost_sorted_arr.cpp
@@ -20,13 +20,39 @@ void insert_heap(int heap[], int curr_end){
     }
 }
 
+void siftdown(int heap[], int heap_end){
+    int p = 0;
+    int c = -1;
+    while(1){
+        c = 2*p+2<=heap_end && heap[2*p+2]<heap[2*p+1]?2*p+2:2*p+1;
+        if(c>heap_end || c<0 || heap[c]>heap[p]) break;
+        swap(heap,c,p);
+        p = c;
+    }
+}
+
 void sort_k_heap(int arr[], int size, int k){
     int heap[k];
-    for(int i=0; i<size && i<k+1; i++){
-        heap[i] = arr[i];
-        insert(heap,i);
+    int curr = 0, curr_sorted=-1;
+    while(curr<size && curr<k+1){
+        heap[curr] = arr[curr];
+        insert_heap(heap,curr);
+        curr++;
     }
-
+    int heap_end = k;
+    curr = k+1;
+    while(1){
+        arr[++curr_sorted] = heap[0];
+        if(curr<size){
+            heap[0] = arr[curr];
+            curr++;
+        }
+        else{
+            if(heap_end == 0) break;
+            heap[0] = heap[heap_end--];
+        }
+        siftdown(heap,heap_end);
+    }
 }
 
 int main(){
