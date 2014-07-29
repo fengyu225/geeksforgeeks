ed84c7932fbafb9c0537b113872732daf8aa6f57
yufeng <fengyu225@gmail.com>
2014-07-29 00:13:55 -0700

    M  search_in_rotated_sorted_array.cpp

--- search_in_rotated_sorted_array.cpp
+++ search_in_rotated_sorted_array.cpp
@@ -1,13 +1,25 @@
 #include "header.h"
 
-int search(int A[], int n, int target) {
+int search(int arr[], int n, int target) {
     if(n==0) return -1;
     int l=0,r=n-1;
     while(l<r){
         int m=(l+r+1)/2;
         if(arr[m] == target) return m;
         if(arr[m] < arr[0]){
-            if(target
+            if(target<arr[m] || target>arr[0])
+                r=m-1;
+            else
+                l = m+1;
+        }
+        if(arr[m] > arr[0]){
+            if(target>arr[m] || target<arr[0])
+                l = m+1;
+            else
+                r = m-1;
+        }
+    }
+    return l==r && arr[l] == target?l:-1;
 }
 
 int main(){
