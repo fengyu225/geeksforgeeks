b8402d7343d7c0c765bc8cacaf61ce6117597fb0
yu <fengyu225@gmail.com>
2014-07-29 17:23:19 -0700

    M  convert_tree_to_ddl_postorder.cpp

--- convert_tree_to_ddl_postorder.cpp
+++ convert_tree_to_ddl_postorder.cpp
@@ -1,19 +1,50 @@
 #include "header.h"
 
 struct TreeNode {
- int val;
- TreeNode *left, *right;
- TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-struct DLLNode {
     int val;
-    DLLNode* prev, *next;
-    DLLNode(int x):val(x),prev(NULL),next(NULL){}
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-DLLNode* convert(TreeNode* root){
-    return NULL;
+void _convert(TreeNode* curr, TreeNode*& start, TreeNode*& end){
+    if(!curr){
+        start = end = NULL;
+        return;
+    }
+    TreeNode* r_s = NULL;
+    TreeNode* r_e = NULL;
+    _convert(curr->right, r_s, r_e);
+    TreeNode* l_s = NULL;
+    TreeNode* l_e = NULL;
+    _convert(curr->left, l_s, l_e);
+    start = l_s?l_s:r_s?r_s:curr; 
+    end = curr;
+    TreeNode* a = NULL;
+    TreeNode* b = NULL;
+    if(r_s){
+        a = r_s;
+        b = curr;
+        r_e->right = curr;
+        curr->left = r_e;
+        r_s->left = curr;
+        curr->right = r_s;
+    }
+    else
+        a = b = curr;
+    if(l_s){
+        l_e->right = a;
+        a->left = l_e;
+        l_s->left = b;
+        b->right = l_s;
+    }
+}
+
+TreeNode* convert(TreeNode* root){
+    if(!root) return NULL;
+    TreeNode* start = NULL;
+    TreeNode* end = NULL;
+    _convert(root,start,end);
+    return start;
 }
 
 int main(){
@@ -28,12 +59,12 @@ int main(){
     n2.left = &n4;
     n2.right = &n5;
     n3.right = &n7;
-    DLLNode* res = convert(&n1);
-    DLLNode* curr = res;
+    TreeNode* res = convert(&n1);
+    TreeNode* curr = res;
     do{
         if(!curr) break;
-        cout<<curr->val<<", left: "<<curr->prev->val<<" right: "<<curr->next->val<<endl;
-        curr = curr->next;
+        cout<<curr->val<<", left: "<<curr->left->val<<" right: "<<curr->right->val<<endl;
+        curr = curr->right;
     }while(curr != res);
     return 0;
 }
