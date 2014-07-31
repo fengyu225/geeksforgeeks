15aa46d02b20b6578a3d8ef88509aece47928fbe
yufeng <fengyu225@gmail.com>
2014-07-31 05:38:49 -0700

    M  find_subarr_with_given_sum.cpp

--- find_subarr_with_given_sum.cpp
+++ find_subarr_with_given_sum.cpp
@@ -1,11 +1,24 @@
 #include "header.h"
 
+/*http://www.geeksforgeeks.org/find-subarray-with-given-sum/
+ */
+
 void find_subarr(int arr[], int size, int sum){
-    
+    int start = 0, res = arr[0];
+    for(int i=1; i<=size; i++){
+        while(res>sum && start<i-1){
+            res -= arr[start];
+            start++;
+        }
+        if(res == sum)
+            cout<<"start: "<<start<<" end "<<i-1<<endl;
+        if(i<size)
+            res += arr[i];
+    }
 }
 
 int main(){
     int arr[] = {1, 4, 20, 3, 10, 5};
-    find_subarr(arr,sizeof(arr)/sizeof(arr[0]), int sum);
+    find_subarr(arr,sizeof(arr)/sizeof(arr[0]), 23);
     return 0;
 }
