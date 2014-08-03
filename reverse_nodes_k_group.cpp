01194a2d7fdce17422f4a13179897d889d9ff6c9
yufeng <fengyu225@gmail.com>
2014-08-03 00:46:29 -0700

    M  reverse_nodes_k_group.cpp

--- reverse_nodes_k_group.cpp
+++ reverse_nodes_k_group.cpp
@@ -21,12 +21,52 @@ ListNode* create_list(int arr[], int n){
     return result;
 }
 
+ListNode* reverse(ListNode* begin, ListNode* end){
+    ListNode* curr = begin;
+    ListNode* next = begin->next;
+    curr->next = NULL;
+    while(next && next != end){
+        ListNode* temp = next->next;
+        next->next = curr;
+        curr = next;
+        next = temp;
+    }
+    return curr;
+}
+
 ListNode *reverseKGroup(ListNode *head, int k) {
+    ListNode* first = head;
+    ListNode* second = head;
+    ListNode* res = NULL;
+    ListNode* last_tail = NULL;
+    while(1){
+        for(int i=0; i<k && second; i++, second = second->next);
+        if(!second){
+            if(res)
+                last_tail->next = first;
+            else
+                res = first;
+            break;
+        }
+        else{
+            ListNode* new_head = reverse(first,second);
+            if(res){
+                last_tail->next = new_head;
+                last_tail = first;
+            }
+            else{
+                res = new_head;
+                last_tail = first;
+            }
+            first = second;
+        }
+    }
+    return res;
 }
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5};
-    //int arr[] = {1};
+//    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {1};
     ListNode* l = create_list(arr, sizeof(arr)/sizeof(arr[0]));
     ListNode* r = reverseKGroup(l,2);
     while(r){
