b91a6e9f351c66ea954e0ba9a5d45c0513fed712
yu <fengyu225@gmail.com>
2014-08-06 15:42:58 -0700

    A  btree_print_diameter.cpp M  generate_excel_header_string.cpp

--- generate_excel_header_string.cpp
+++ generate_excel_header_string.cpp
@@ -17,12 +17,13 @@ string get(int n){
 }
 
 int main(){
-    std::cout<<get(26)<<std::endl;
-    std::cout<<get(51)<<std::endl;
-    std::cout<<get(52)<<std::endl;
-    std::cout<<get(80)<<std::endl;
-    std::cout<<get(676)<<std::endl;
-    std::cout<<get(702)<<std::endl;
-    std::cout<<get(705)<<std::endl;
+    std::cout<<get(26*26)<<std::endl;
+//    std::cout<<get(26)<<std::endl;
+//    std::cout<<get(51)<<std::endl;
+//    std::cout<<get(52)<<std::endl;
+//    std::cout<<get(80)<<std::endl;
+//    std::cout<<get(676)<<std::endl;
+//    std::cout<<get(702)<<std::endl;
+//    std::cout<<get(705)<<std::endl;
     return 0;
 }
