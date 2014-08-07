4e1bf7e625fd53ea5a04a3fe7ca6ad8c08c85f4b
yufeng <fengyu225@gmail.com>
2014-08-06 23:32:58 -0700

    M  btree_print_diameter.cpp ?? x

--- btree_print_diameter.cpp
+++ btree_print_diameter.cpp
@@ -6,15 +6,39 @@ struct TreeNode {
  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void get_diameter_helper(TreeNode* root, vector<TreeNode*>& left_longest_path, vector<TreeNode*>& right_longest_path, vector<TreeNode*>& res){
-
+void get_diameter_helper(TreeNode* root, vector<TreeNode*>& longest_path, vector<TreeNode*>& res){
+    if(!root){
+        longest_path.clear();
+        res.clear();
+        return;
+    }
+    vector<TreeNode*> l_long;
+    vector<TreeNode*> r_long;
+    vector<TreeNode*> l_res;
+    vector<TreeNode*> r_res;
+    get_diameter_helper(root->left,l_long,l_res);
+    get_diameter_helper(root->right,r_long,r_res);
+    vector<TreeNode*> temp = l_res.size()>r_res.size()?l_res:r_res;
+    if(temp.size()>l_long.size()+r_long.size()+1)
+        res = temp;
+    else{
+        vector<TreeNode*> x = l_long;
+        std::reverse(x.begin(), x.end());
+        x.push_back(root);
+        x.insert(x.end(), r_long.begin(), r_long.end());
+        res = x;
+    }
+    if(l_long.size()>r_long.size())
+        longest_path = l_long;
+    else
+        longest_path = r_long;
+    longest_path.insert(longest_path.begin(),root);
 }
 
 vector<TreeNode*> get_diameter(TreeNode* root){
     vector<TreeNode*> res;
-    vector<TreeNode*> left_longest_path;
-    vector<TreeNode*> right_longest_path;
-    get_diameter_helper(root,left_longest_path,right_longest_path,res);
+    vector<TreeNode*> longest_path;
+    get_diameter_helper(root,longest_path,res);
     return res;
 }
 
